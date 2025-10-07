60bd7302b9d9687a7bfb66c32bf97a43079d8308
Long Nguyễn (Ryan) <121480643+ndlryan@users.noreply.github.com>
2025-10-07 17:57:23 +0700

    Delete 0.Getfile.py

--- 0.Getfile.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-url = "https://raw.githubusercontent.com/yinghaoz1/tmdb-movie-dataset-analysis/master/tmdb-movies.csv"
-df = pd.read_csv(url)
-
-df.to_csv('tmdb-movies.csv', index=False)
-
-print("Saved as tmdb-movies.csv")
